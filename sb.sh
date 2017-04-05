be6e6b5f22552657ec2f7ad92a768bb9e89f317c
Alex MacRae <alex.finlay.macrae@gmail.com>
2017-04-05 20:06:12 +1000

    Housekeeping

--- sb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# sb.sh
-# update rc.local:
-# >>> sudo nano /etc/rc.local
-# before exit 0:
-# >>> sudo /home/pi/SamplerBox/sb.sh &
-cd /home/pi/SamplerBox
-python setup.py build_ext --inplace
-python samplerbox.py
